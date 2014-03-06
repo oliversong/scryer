8c9e3bfe0090538282b8c3238171e7f39a1e4122
Oliver Song <osong@mit.edu>
2014-03-06 00:05:10 -0500

    Hm do we need to own all the nodes we communicate with?

--- src/nodeservice/server.go
+++ src/nodeservice/server.go
@@ -2,9 +2,16 @@ package main
 
 import (
   "fmt"
+  "github.com/conformal/btcjson"
 )
 
 func main() {
   // Hello
   fmt.Println("Hello world")
+  msg, err := btcjson.CreateMessage("getinfo")
+  if err != nil {
+    // Handle some error
+  }
+  reply, err := btcjson.RpcCommand("username", "password", "localhost:8332", msg)
+  fmt.Println(reply)
 }
